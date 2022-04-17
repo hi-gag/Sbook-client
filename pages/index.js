8ca99ec19d8fa2b96ecd2400b691b79f29c91470
Max Kim <hwaseen@gmail.com>
2022-04-17 22:16:10 +0900

    샘플 코드, 리드미 수정

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Typography } from 'antd';
+import { Typography, Button, Switch, Input, Space, Card } from 'antd';
 
 export default function Home() {
   return (
@@ -10,6 +10,15 @@ export default function Home() {
       <h1 className="text-2xl font-bold underline">랜딩 페이지</h1>
       <Typography.Text className="underline">텍스트는 이렇게</Typography.Text>
       <Typography.Paragraph>텍스트는 이렇게 해주세용</Typography.Paragraph>
+      <Typography.Paragraph>
+        antd에서 그대로 쓸수 있는거 목록
+      </Typography.Paragraph>
+      <Space className="w-64" direction="vertical" space={10}>
+        <Button type="primary">버튼</Button>
+        <Switch checkedChildren="켜짐" unCheckedChildren="꺼짐" />
+        <Input value="인풋" />
+        {/* 카드는 하나 새로 만들어야댐 */}
+      </Space>
     </>
   );
 }
