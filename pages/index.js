d40bd967b7535b551b81a561dcf4bafc4bfe03d0
Max Kim <hwaseen@gmail.com>
2022-04-24 01:10:57 +0900

    리드미 수정

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,8 @@ export default function Home() {
         <title>Sbook</title>
       </Head>
       <h1 className="text-2xl font-bold underline">랜딩 페이지</h1>
-      <Typography.Text className="underline">텍스트는 이렇게</Typography.Text>
-      <Typography.Paragraph>텍스트는 이렇게 해주세용</Typography.Paragraph>
-      <Typography.Paragraph>
-        antd에서 그대로 쓸수 있는거 목록
-      </Typography.Paragraph>
+      <div className="underline">텍스트는 이렇게</div>
+      <div>antd에서 그대로 쓸수 있는거 목록</div>
       <Space className="w-64" direction="vertical" space={10}>
         <Button type="primary">버튼</Button>
         <Switch checkedChildren="켜짐" unCheckedChildren="꺼짐" />
