8bae7c09691b8851802fa390fbe4d4d9d96c217c
soyeonysoss <qkrdus23@gmail.com>
2022-05-03 21:50:21 +0900

    api 사용 코드 추가

--- components/form/LoginForm.jsx
+++ components/form/LoginForm.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Form,
   Input,
   Button,
 } from 'antd';
 import "antd/dist/antd.css";
+import axios from "axios";
+import Router from 'next/router';
 
 // antd 레이아웃
 const formItemLayout = {
@@ -39,21 +41,42 @@ const tailFormItemLayout = {
 };
 
 const LoginForm = () => {
+  const [errMsg, setErrMsg] = useState(false);
+
+  // axios
+  const submit = (values) => {
+    const option = {
+      method: "POST",
+      url: "http://34.64.159.191:8081/user/login",
+      data: values,
+    };
+    axios(option)
+    .then((res) => {
+      console.log(res.data);
+      const status = res.data.code;
+      if (status === "200") { // 로그인 성공, 토큰 저장
+        const token = res.data.data.token;
+        localStorage.setItem('jwt', token);
+        Router.push('/bookmark')
+      } 
+      else if (status === "401") { // 존재하지 않는 아이디 비번
+        setErrMsg(true)
+      }
+    })
+    .catch()
+  };
 
   // form 
   const [form] = Form.useForm();
-
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values);
-  };
 
   return (
     <>
       <Form
         {...formItemLayout}
         form={form}
-        name="register"
-        onFinish={onFinish}
+        name="register"      
+        onFinish={submit}
+        // onFinish={onFinish}
         scrollToFirstError
       >
         <Form.Item
@@ -82,8 +105,13 @@ const LoginForm = () => {
         >
           <Input.Password />
         </Form.Item>
-
         <Form.Item {...tailFormItemLayout}>
+          {errMsg && 
+            <div className="mb-6">
+              <div className='text-[#ff4d4f]'>존재하지 않는 ID와 비밀번호입니다.</div>
+              <div className="inline-block text-[#ff4d4f] mr-3">회원가입 하시겠습니까?</div>
+              <a className="inline-block" href="">회원가입</a>
+            </div>}
           <Button type="primary" htmlType="submit">
             로그인
           </Button>
