89a429251c7ca8bc311cee69a5c432c7b7e942fb
Soyeon Park <86656217+soyeonysoss@users.noreply.github.com>
2022-06-05 11:11:52 +0900

    bookmarkListList 가져오는 부분

--- pages/bookmark/index.jsx
+++ pages/bookmark/index.jsx
@@ -1,26 +1,24 @@
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
-import { mockBookmarkListList } from '../../mock';
 import { BookmarkList } from '../../components/bookmark/BookmarkList';
+import { useQuery } from 'react-query';
 
-export async function getServerSideProps() {
-  return {
-    props: {
-      bookmarkListList: mockBookmarkListList,
-    },
-  };
-}
-
-function BookmarkDetail({ bookmarkId, bookmarkListList }) {
+function BookmarkDetail({ bookmarkId }) {
+  const {
+    data: bookmarkListList,
+    isLoading: isBookmarkListListLoading,
+    isError: isBookmarkListListError,
+  } = useQuery('bookmarkList');
+  
   const [auth, setAuth] = useState({
     username: '',
     isAuth: false,
   });
 
   useEffect(() => {
-    const jwt = window.sessionStorage.getItem('jwt');
-    const username = window.sessionStorage.getItem('username');
+    const jwt = window.localStorage.getItem('jwt');
+    const username = window.localStorage.getItem('username');
 
     setAuth({
       username,
@@ -37,10 +35,12 @@ function BookmarkDetail({ bookmarkId, bookmarkListList }) {
         <div className="content-wrapper">
           {auth.isAuth ? (
             <>
-              <BookmarkList
-                bookmarkListList={bookmarkListList}
-                bookmarkId={bookmarkId}
-              />
+              {!isBookmarkListListLoading && !isBookmarkListListError && (
+                <BookmarkList
+                  bookmarkListList={bookmarkListList.data.data}
+                  bookmarkId={bookmarkId}
+                />
+              )}
               <div className="text-center text-xl">
                 상단의 북마크를 선택해주세요
               </div>
