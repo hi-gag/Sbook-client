0ca50622371d5ae155509619c3116737a3f408b5
Max Kim <hwaseen@gmail.com>
2022-06-10 00:58:10 +0900

    추가 fallback

--- components/bookmark/BookmarkTitle/BookmarkHeader.jsx
+++ components/bookmark/BookmarkTitle/BookmarkHeader.jsx
@@ -6,12 +6,14 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useQueryClient } from 'react-query';
 import { postBookmarkTopic, postBookmark } from '../../../api';
+import { useState } from 'react';
 
 function BookmarkHeader({ shared, bookmarkId }) {
   const router = useRouter();
   const [viewMode, setViewMode] = useRecoilState(bookmarkViewModeAtom);
   const queryClient = useQueryClient();
   const [form] = Form.useForm();
+  const [loadingFallback, setLoadingFallback] = useState('주소를 입력해주세요');
 
   const handleChange = () => {
     setViewMode((mode) => (mode === 'normal' ? 'memo' : 'normal'));
@@ -20,20 +22,27 @@ function BookmarkHeader({ shared, bookmarkId }) {
   const submitNewBookmark = async () => {
     const { url } = form.getFieldValue();
     const token = window.localStorage.getItem('jwt') ?? '';
+    setLoadingFallback('본문을 파싱하고 키워드를 도출합니다');
 
+    try {
+      const {
+        data: { bookmarkListId, ...bookmarkBody },
+      } = await postBookmarkTopic(bookmarkId, url);
+      setLoadingFallback('도출한 키워드와 본문을 서버로 전달합니다');
+
+      // 내용을 자바서버에 넘기기
+      await postBookmark(token, bookmarkId, {
+        ...bookmarkBody,
+        importance: 1,
+      });
+
+      // 쿼리 리프레시(북마크 ID 필요함)
+      queryClient.invalidateQueries(`bookmark-${bookmarkListId}`);
+      setLoadingFallback('북마크 추가가 완료되었습니다');
+    } catch (e) {
+      setLoadingFallback('오류가 발생했습니다');
+    }
     // 토픽서버에 URL 넘기기
-    const {
-      data: { bookmarkListId, ...bookmarkBody },
-    } = await postBookmarkTopic(bookmarkId, url);
-
-    // 내용을 자바서버에 넘기기
-    await postBookmark(token, bookmarkId, {
-      ...bookmarkBody,
-      importance: 1,
-    });
-
-    // 쿼리 리프레시(북마크 ID 필요함)
-    queryClient.invalidateQueries(`bookmark-${bookmarkListId}`);
   };
 
   return (
@@ -42,18 +51,22 @@ function BookmarkHeader({ shared, bookmarkId }) {
         <Checkbox checked={shared} />
         <span className="ml-2">공유 여부</span>
       </div>
-      <Form
-        className="flex space-between section-width"
-        form={form}
-        name="bookmark-list-create"
-      >
-        <Form.Item name="url">
-          <Input ref placeholder="추가할 URL 입력" />
-        </Form.Item>
-        <Button className="ml-4" type="primary" onClick={submitNewBookmark}>
-          입력
-        </Button>
-      </Form>
+      <div className="relative">
+        <Form
+          className="flex space-between section-width"
+          form={form}
+          name="bookmark-list-create"
+        >
+          <Form.Item name="url">
+            <Input ref placeholder="추가할 URL 입력" />
+          </Form.Item>
+          <Button className="ml-4" type="primary" onClick={submitNewBookmark}>
+            입력
+          </Button>
+        </Form>
+        <div className="absolute text-center">{loadingFallback}</div>
+      </div>
+
       <div className="flex space-between justify-end section-width">
         <div className="mr-4">
           <Switch
